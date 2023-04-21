a7459ac318b5964f71e184ea78373a53780c24a6
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-04-21 22:10:22 +0300

    1.2. Вызов метода объекта [#166085]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,14 +1,14 @@
 package ru.job4j.oop;
 
-import java.util.StringJoiner;
-
 public class Student {
     public void music(String lyrics) {
         System.out.println("I can sing a song: " + lyrics);
     }
+
     public void sing() {
         System.out.println("I believe I can fly");
     }
+
     public static void main(String[] args) {
         Student petya = new Student();
         String song = "I believe I can fly";
