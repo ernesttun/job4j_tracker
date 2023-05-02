caf7ef12a564931097f947cdf133b10056016601
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-02 21:55:45 +0300

    6.2. Локальные и анонимные внутренние классы [#504952]

---
+++ src/main/java/ru/job4j/oop/Local.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Local {
+    private String name = "Ernest";
+
+    public void getFullName() {
+        final String surname = "Tyunyan";
+
+        class FullName{
+            public void printFullName() {
+                System.out.println(name + " " + surname);
+            }
+        }
+        FullName fullName = new FullName();
+        fullName.printFullName();
+    }
+
+    public static void main(String[] args) {
+        Local local = new Local();
+        local.getFullName();
+    }
+}
