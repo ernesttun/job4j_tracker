4794e5ad5b09ccb878b8fd8012abd313cd17741b
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-06 19:48:20 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,17 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+            student.setName("Иванов");
+            student.setSurName("Иван");
+            student.setPatronymic("Иванович");
+            student.setGroup(117);
+            student.setDateOfReceipt(new Date());
+
+        System.out.println("ФИО: " + student.getName() + " " + student.getSurName() +
+                " " + student.getPatronymic() + " has group " + student.getGroup());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,55 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String name;
+
+    private String surName;
+
+    private String patronymic;
+
+    private int group;
+
+    private Date dateOfReceipt;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public Date getDateOfReceipt() {
+        return dateOfReceipt;
+    }
+
+    public void setDateOfReceipt(Date dateOfReceipt) {
+        this.dateOfReceipt = dateOfReceipt;
+    }
+}
