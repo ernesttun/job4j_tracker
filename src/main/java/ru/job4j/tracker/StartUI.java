8d8d0f3a8822355cc228f7fb78c9cad801a85658
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-13 21:33:06 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String formattedDateTime = created.format(formatter);
         System.out.println(formattedDateTime);
+        Item it = new Item();
+        System.out.println(it);
     }
 }
