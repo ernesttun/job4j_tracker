970fe7a24b66e9002a7ffd01649b79c5f6f27b22
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-20 20:53:30 +0300

    4.0. Что такое Interface. [#504946]

---
+++ src/main/java/ru/job4j/polymorph/Main.java
@@ -0,0 +1,14 @@
+package ru.job4j.polymorph;
+
+import ru.job4j.oop.Sportsmen;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle sportCar = new SportCar();
+        sportCar.accelerate();
+        sportCar.brake();
+        sportCar.changeGear();
+        sportCar.steer();
+        sportCar.refill();
+    }
+}
