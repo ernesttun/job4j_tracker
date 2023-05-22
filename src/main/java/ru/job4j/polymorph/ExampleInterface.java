3099f294a56f29f36e57dcb1e59e23cfb4a00e12
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-22 21:59:35 +0300

    4.1. Элементы интерфейса [#504948]

--- src/main/java/ru/job4j/polymorph/ExampleInterface.java
+++ src/main/java/ru/job4j/polymorph/ExampleInterface.java
@@ -2,10 +2,10 @@ package ru.job4j.polymorph;
 
 public interface ExampleInterface {
     class ExampleClass {
-//        ... код класса ...
+
     }
 
     interface InnerExampleInterface {
-//        ... код интерфейса ...
+
     }
 }
