a3ef06429be70bb491f255b9675f7a438c71f104
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-25 22:15:09 +0300

    8. Input и полиморфизм. [#181094]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,12 +6,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StartUITest {
     @Test
     public void whenAddItem() {
-        String[] answer = {"Вор в законе"};
+        String[] answer = {"Ernest"};
         Input input = new StubInput(answer);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
-        Item excepted = new Item("Вор в законе");
+        Item excepted = new Item("Ernest");
         assertThat(created.getName()).isEqualTo(excepted.getName());
     }
 }
