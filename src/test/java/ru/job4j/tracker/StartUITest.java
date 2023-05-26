50aa0549f9af65dccfca981965ca3331a7a9f684
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-26 21:11:48 +0300

    9. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,4 +14,24 @@ class StartUITest {
         Item excepted = new Item("Ernest");
         assertThat(created.getName()).isEqualTo(excepted.getName());
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Er");
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId()), "Ernest"};
+        StartUI.editItem(new StubInput(answer), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("Ernest");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Vor v zakone");
+        tracker.add(item);
+        boolean delete = tracker.delete(item.getId());
+        assertThat(delete).isTrue();
+    }
 }
