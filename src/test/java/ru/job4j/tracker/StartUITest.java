e31c983ce3bdf598f9391e05a6af2574f5f800a8
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-26 22:20:58 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,34 +4,4 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
-    @Test
-    public void whenAddItem() {
-        String[] answer = {"Ernest"};
-        Input input = new StubInput(answer);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item excepted = new Item("Ernest");
-        assertThat(created.getName()).isEqualTo(excepted.getName());
-    }
-
-    @Test
-    public void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("Er");
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId()), "Ernest"};
-        StartUI.editItem(new StubInput(answer), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("Ernest");
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("Vor v zakone");
-        tracker.add(item);
-        boolean delete = tracker.delete(item.getId());
-        assertThat(delete).isTrue();
-    }
 }
