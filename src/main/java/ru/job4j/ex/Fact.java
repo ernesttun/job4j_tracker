10ddf30bbe22ad8151dd806919d63f656257ff58
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-05-31 22:59:59 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N could not be less than 0");
+        }
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
