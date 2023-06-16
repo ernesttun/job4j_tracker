a8ef9d70eede9864517d30ac7f7e8062eed7460d
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-06-16 23:26:04 +0300

    1. Singleton [#94606  [#94606]]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -33,7 +33,6 @@ public final class SingleTracker {
         return tracker.findByName(key);
     }
 
-
     public boolean replace(int id, Item item) {
         return tracker.replace(id, item);
     }
