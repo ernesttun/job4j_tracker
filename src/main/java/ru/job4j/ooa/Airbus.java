e2709b5ddffcbcc7078a3977edcc991991502d18
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-06-19 19:09:31 +0300

    3. final [#334073  [#334073]]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends AirCraft {
     }
 
     public void printCountEngine() {
-        int numberOfEngines = name.equals("A380") ? 4 : COUNT_ENGINE;
+        int numberOfEngines = "A380".equals(name) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + numberOfEngines);
     }
 
