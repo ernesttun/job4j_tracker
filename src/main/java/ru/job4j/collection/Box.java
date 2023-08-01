ac970b097e1e0a90a936bce642f16da6f15909bf
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-08-01 17:34:03 +0300

    3. Автоупаковка / автораспаковка [#504971 # [#504971 #433899]]

---
+++ src/main/java/ru/job4j/collection/Box.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+public class Box {
+    public static void num(Integer x) {
+        System.out.println("Значение типа Integer: " + x);
+    }
+
+    public static void num(int x) {
+        System.out.println("Значение типа int: " + x);
+    }
+
+
+    public static void main(String[] args) {
+        Integer g = 250;
+        Integer h = 250;
+        System.out.println(g == h);
+
+        Integer v = 125;
+        Integer w = 125;
+        System.out.println(v == w);
+        int m = 2;
+        num(m);
+    }
+}
