e6223d709519586337aec86b3cf9c50cdbe3fd4b
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-08-01 18:26:18 +0300

    4. Телефонный справочник на базе ArrayList [#41598  [#41598]]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> person = new ArrayList<>();
+
+    public void add(Person person) {
+        this.person.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> rsl = new ArrayList<>();
+        for (Person itr : person) {
+            if (key.contains(itr.getName()) || key.contains(itr.getSurname()) ||
+            key.contains(itr.getPhone()) || key.contains(itr.getAddress())) {
+                rsl.add(itr);
+            }
+        }
+        return rsl;
+    }
+}
