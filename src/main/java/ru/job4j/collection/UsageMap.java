6cdbc5d055c57f083dba9559449b6aa23ef706bf
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-09-13 23:51:07 +0300

    0. Map, HashMap [#212652 #444683]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("e.tunan11@gmail.com", "Ernest Tyunyan");
+        map.put("e.tun@gmail.com", "Ernest");
+        map.put("e.q@mail.ru", "Er");
+        map.put("e.tunan11@gmail.com", "Nest");
         for (String itr : map.keySet()) {
             String value = map.get(itr);
             System.out.println(itr + " = " + value);
