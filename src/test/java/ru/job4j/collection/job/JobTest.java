bbd5bd2603885e134ee22f1048b6c9a0c19f2d9c
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-09-29 01:19:45 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/job/JobTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.collection.job;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JobTest {
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorAscByName() {
+        Comparator<Job> comparator = new JobAscByName();
+        int rsl = comparator.compare(
+                new Job("Ernest", 0),
+                new Job("Messi", 1)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+    @Test
+    public void whenComparatorAscByPriority() {
+        Comparator<Job> comparator = new JobAscByPriority();
+        int rsl = comparator.compare(
+                new Job("Ernest", 0),
+                new Job("Messi", 1)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorDescByName() {
+        Comparator<Job> comparator = new JobDescByName();
+        int rsl = comparator.compare(
+                new Job("Messi", 1),
+                new Job("Ernest", 0)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorDescByPriority() {
+        Comparator<Job> comparator = new JobDescByPriority();
+        int rsl = comparator.compare(
+                new Job("Messi", 3),
+                new Job("Ernest", 2)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorAscByNameByPriorityAndDescByNameByPriority() {
+        Comparator<Job> comparator = new JobAscByName().thenComparing(new JobAscByPriority()).
+                thenComparing(new JobDescByName()).thenComparing(new JobDescByPriority());
+        int rsl = comparator.compare(
+                new Job("Impl task", 5),
+                new Job("Reboot server", 8)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+}
