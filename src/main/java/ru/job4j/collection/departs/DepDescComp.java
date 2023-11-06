d84f51866d844abf736b91d281befa5c3bb87c2e
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-11-07 00:21:54 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/departs/DepDescComp.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection.departs;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] one = o1.split("/");
+        String[] two = o2.split("/");
+        if(two[0].compareTo(one[0]) == 0) {
+            return o1.compareTo(o2);
+        }
+        return two[0].compareTo(one[0]);
+    }
+}
