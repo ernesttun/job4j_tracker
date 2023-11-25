714215661409684a6e2be8433a1e3801a8c02d96
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-11-25 22:07:18 +0300

    9. Лямбда выражения и Collection Framework [#505026]

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -34,9 +34,10 @@ public class AnalyzeByMap {
 
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                String subjectName = subject.name();
-                int score = subject.score();
-                map.put(subjectName, map.getOrDefault(subjectName, 0) + score);
+//                String subjectName = subject.name();
+//                int score = subject.score();
+//                map.put(subjectName, map.getOrDefault(subjectName, 0) + score);
+                map.merge(subject.name(), subject.score(), (oldValue, newValue) -> oldValue + subject.score());
             }
         }
 
@@ -71,9 +72,7 @@ public class AnalyzeByMap {
 
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                String subjectName = subject.name();
-                int score = subject.score();
-                map.put(subjectName, map.getOrDefault(subjectName, 0) + score);
+                map.merge(subject.name(), subject.score(), (oldValue, newValue) -> oldValue + subject.score());
             }
         }
 
