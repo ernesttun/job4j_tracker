725bf6b201e255f08b876f1f7116b978b0f1c875
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-12-02 23:35:38 +0300

    6. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(profile -> new Address(profile.getAddress().getCity(),
+                        profile.getAddress().getStreet(),
+                        profile.getAddress().getHome(),
+                        profile.getAddress().getApartment()))
+                .collect(Collectors.toList());
+    }
+}
